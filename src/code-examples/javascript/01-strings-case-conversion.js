0bb4f352fd4462013789b3a6dfcb7eb17d1c5ac6
sathsure <adamsathish24@yahoo.com>
2026-01-02 10:40:28 +0530

    Remove outdated images and JavaScript code examples for various concepts

--- src/code-examples/javascript/01-strings-case-conversion.js
+++
@@ -1,8 +0,0 @@
-//convert a string from camelCase to snake_case
-
-let str = "oneCompilerPlatform";
-
-const toSnakeCase = str.replace(/([A-Z])/g, "_$1").toLowerCase();
-
-console.log(toSnakeCase);
-// Expected output: one_compiler_platform
--- src/code-examples/javascript/02-arrays-second-largest-number.js
+++
@@ -1,7 +0,0 @@
-let array = [1, 3, 5, 7, 9, 10];
-
-const newArray = array.sort((a, b) => a - b);
-
-console.log(newArray[newArray.length - 2]);
-
-// Output: 9
